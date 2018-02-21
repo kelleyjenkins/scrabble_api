97f51f50642231b1dcc086c83dea40e7fdb3ac81
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-21 09:19:38 -0700

    added search box to root page, created test for feature 1

---
+++ spec/features/user_can_validate_word_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "user validates word" do
+  it "enters a word and returns message" do
+    visit '/'
+
+    fill_in(:search, :with => "foxes")
+    click_on "Validate Word"
+
+    expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'")
+  end
+end
+
+
+# As a user
+# When I visit "/"
+# And I fill in a text box with "foxez"
+# And I click "Validate Word"
+# Then I should see a message that says "'foxez' is not a valid word."
