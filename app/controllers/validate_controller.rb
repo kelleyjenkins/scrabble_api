bcc327084261b428cc7ec972f109b0d030f8940a
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-24 10:22:31 -0700

    step 2 - knew I needed @word in controller to access from view, because I am calling message method, need to create that in a word model, so set @word = Word.new to go from there

--- app/controllers/validate_controller.rb
+++ app/controllers/validate_controller.rb
@@ -1,19 +1,20 @@
 class ValidateController < ApplicationController
   def index
-    @word = params[:search]
+    @word = Word.new(params[:search])
+    # @word = params[:search]
     # @result = OxfordService.new(@word).validation
 
-    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
-      faraday.headers['app_id'] = ENV["oxford_id"]
-      faraday.headers['app_key'] = ENV["oxford_key"]
-      faraday.adapter Faraday.default_adapter
-    end
-
-      @response = @conn.get("/api/v1/inflections/en/#{@word}")
-
-      unless @response.status == 404
-        @result = JSON.parse(@response.body, symbolize_names: true)[:results].first[:lexicalEntries].first[:inflectionOf].first[:text]
-      end
-
+  #   @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
+  #     faraday.headers['app_id'] = ENV["oxford_id"]
+  #     faraday.headers['app_key'] = ENV["oxford_key"]
+  #     faraday.adapter Faraday.default_adapter
+  #   end
+  #
+  #     @response = @conn.get("/api/v1/inflections/en/#{@word}")
+  #
+  #     unless @response.status == 404
+  #       @result = JSON.parse(@response.body, symbolize_names: true)[:results].first[:lexicalEntries].first[:inflectionOf].first[:text]
+  #     end
+  #
   end
 end
