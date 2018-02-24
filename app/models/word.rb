1c574c11b37a56ab5af548f8a572657f554f16c5
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-24 10:24:22 -0700

    created word PORO that takes in the searched word and definied message method. at this point i needed a way to validate if the word is legit, so running word through the validation service

---
+++ app/models/word.rb
@@ -0,0 +1,20 @@
+class Word
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def message
+    if root == false
+      "'#{@word}' is not a valid word"
+    else
+    "'#{@word}' is a valid word and its root form is '#{root}'"
+    end
+  end
+
+  def root
+     ValidateService.new(@word).find_root
+  end
+
+end
